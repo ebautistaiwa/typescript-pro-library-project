0e408d2b87c34d954a493058db20e41ea8d6caac
alzuhara <ebautista@iwa.com.mx>
2017-10-21 19:39:55 -0500

    Publicar versión 1.1.0

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,20 @@
-import lib from './index'
+import { isBoolean } from './index'
 
-test('it should work', () => {
-  expect(true).toEqual(true)
-  console.log(lib)
+test('isBoolean should return true if the given value is an instance of Boolean', () => {
+  expect(isBoolean(new Boolean())).toEqual(true)
+})
+
+test('isBoolean should return true if the given value is the litera `true`', () => {
+  expect(isBoolean(true)).toEqual(true)
+})
+
+test('isBoolean should return true if the given value is the litera `false`', () => {
+  expect(isBoolean(false)).toEqual(true)
+})
+
+test('isBoolean should return false if the given value is any non-boolean value', () => {
+  expect(isBoolean('')).toEqual(false)
+  expect(isBoolean(undefined)).toEqual(false)
+  expect(isBoolean(null)).toEqual(false)
+  expect(isBoolean(123)).toEqual(false)
 })
